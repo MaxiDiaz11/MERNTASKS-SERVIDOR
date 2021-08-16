2a85e7cde1662b07839067230f3497901e78abef
Diaz Daniel Maximiliano <50111435+MaxiDiaz11@users.noreply.github.com>
2021-08-16 16:52:06 -0300

    Se valida si existe un usuario en BD y se encripta la contraseña

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,18 +1,32 @@
 const Usuario = require('../models/Usuario')
+const bcryptjs = require('bcryptjs')
 
 exports.crearUsuario = async (req, res) => {
+
+    //extraer email y password
+    const { email, password } = req.body;
+
     try {
-        let usuario;
+        //Revisar que el usuario registrado sea unico
+        let usuario = await Usuario.findOne({ email })
+
+        if (usuario) {
+            return res.status(400).json({ msg: 'El usuario ya existe' })
+        }
         //crea nuevo usuario
         usuario = new Usuario(req.body);
+
+        //Hashear el password
+        const salt = await bcryptjs.genSalt(10);
+        usuario.password = await bcryptjs.hash(password, salt)
 
         //guardar usuario
         await usuario.save();
 
         //mensaje de confirmacion
-        res.send("Usuario creado correctamente")
+        res.status(200).json({ msg: 'Usuario creado correctamente' })
         console.log("usuario guardado")
-        
+
     } catch (error) {
         console.log(error)
         res.status(400).send("Hubo un error")
