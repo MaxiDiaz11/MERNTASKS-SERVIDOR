6290073a1b1b89863dbee0fad0e0b5b4702cd7fc
Diaz Daniel Maximiliano <50111435+MaxiDiaz11@users.noreply.github.com>
2021-08-16 17:06:19 -0300

    Se agrego express-validator

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,9 +2,14 @@
 const express = require('express')
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController')
+const { check } = require('express-validator');
 
 //crea un usuario
 //api/usuarios
-router.post('/', usuarioController.crearUsuario);
+router.post('/', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'Agrega un email valido').isEmail(),
+    check('password', 'El password debe ser minimo de 6 caracteres').isLength(6),
+], usuarioController.crearUsuario);
 
 module.exports = router;
