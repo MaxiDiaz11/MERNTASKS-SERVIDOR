4e4220fd5fe2b1a8e88488cedfddd341d75624f2
Diaz Daniel Maximiliano <50111435+MaxiDiaz11@users.noreply.github.com>
2021-08-18 11:50:02 -0300

    creacion de proyecto terminada

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -1,12 +1,17 @@
 const Proyecto = require('../models/Proyecto')
 
-exports.crearProyecto = async(req, res) =>{
+exports.crearProyecto = async (req, res) => {
 
     try {
         //crear un nuevo proyecto
         const proyecto = new Proyecto(req.body);
+
+        //guardar el creador via jwt
+        proyecto.creador = req.usuario.id
+
+        //guardar el proyecto
         await proyecto.save();
-        res.json(proyecto)
+        res.status(200).json({ msg: 'proyecto creado correctamente' })
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error')
--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const proyectosController = require('../controllers/proyectosController')
+const auth = require('../middleware/auth')
 
 //Crea proyectos
 //api/proyectos
-router.post('/', proyectosController.crearProyecto)
+router.post('/', auth, proyectosController.crearProyecto)
 
 module.exports = router;
