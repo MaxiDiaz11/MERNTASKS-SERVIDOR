03d0840e53ed0a8462d4c9bb392f00576ad73867
Diaz Daniel Maximiliano <50111435+MaxiDiaz11@users.noreply.github.com>
2021-08-18 12:00:39 -0300

    Se agrego validacion antes de agregar un proyecto

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const proyectosController = require('../controllers/proyectosController')
 const auth = require('../middleware/auth')
+const { check } = require('express-validator')
 
 //Crea proyectos
 //api/proyectos
-router.post('/', auth, proyectosController.crearProyecto)
+router.post('/', auth, [
+    check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+], proyectosController.crearProyecto)
 
 module.exports = router;
